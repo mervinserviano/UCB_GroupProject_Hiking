04258c237740552b47908d64e4398ac1d66f81c1
wick7 <wickersham.craig@gmail.com>
2018-11-05 17:44:44 -0800

    changed trail status to trail summary

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -82,7 +82,7 @@
                 <div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Status: ${response.trails[i].conditionStatus}</li>
-                        <li class="list-group-item">Condition: ${response.trails[i].conditionDetails}</li>
+                        <li class="list-group-item">Summary: ${response.trails[i].summary}</li>
                         <li class="list-group-item">Difficulty: ${response.trails[i].difficulty}</li>
                         <li class="list-group-item">Stars: ${response.trails[i].stars}<li>
                     </ul>
@@ -104,7 +104,7 @@
                 <div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Status: ${response.trails[i].conditionStatus}</li>
-                        <li class="list-group-item">Condition: ${response.trails[i].conditionDetails}</li>
+                        <li class="list-group-item">Summary: ${response.trails[i].summary}</li>
                         <li class="list-group-item">Difficulty: ${response.trails[i].difficulty}</li>
                         <li class="list-group-item">Stars: ${response.trails[i].stars}<li>
                     </ul>
